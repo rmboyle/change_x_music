832e7756212e319fe907cdc6c88dc13806d8d260
rmboyle <rmichaelboyle@gmail.com>
2013-06-10 23:21:21 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChangeXMusic::Application.config.secret_token = '5d628e4d2bac8a84eaa7008f68c6722d05a33c1f7c84b383a13e97cc853407942fd599586c8774d920c5a0a92e09d5e696ebd1a5760f5027dc206864c009bc34'
